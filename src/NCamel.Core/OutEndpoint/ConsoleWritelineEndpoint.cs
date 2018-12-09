34eae4051fec482326cf8c301d2bba50c8697810
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2018-12-09 12:48:16 +0100

    add ConsoleWritelineEndpoint

---
+++ src/NCamel.Core/OutEndpoint/ConsoleWritelineEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace NCamel.Core
+{
+    public class ConsoleWritelineEndpoint
+    {
+        private readonly Context ctx;
+
+        public ConsoleWritelineEndpoint(Context ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public void Execute(Exchange e)
+        {
+            Console.WriteLine("ConsoleWritelineEndpoint");
+            Console.WriteLine($"id:{e.Message.Id}");
+            Console.WriteLine("Headers");
+            e.Message.MetaData.ToList().ForEach(x => Console.WriteLine(x.Key + "=" + x.Value.ToString()));
+            Console.WriteLine($"content:{e.Message.Content}");
+        }
+    }
+}
